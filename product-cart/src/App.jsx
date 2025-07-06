93b51684679fab57af5d908c8753ee2636948845
olawale3334 <olawale3334@gmail.com>
2025-07-06 14:08:26 +0100

    product  adding using props

--- product-cart/src/App.jsx
+++ product-cart/src/App.jsx
@@ -3,19 +3,94 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
 import Card from './components/page'
-import brownie from '/image-brownie-desktop.jpg'
+
+import brownie from '/image-brownie-mobile.jpg'
+import brownie2 from '/image-brownie-desktop.jpg'
+import waffle from '/image-waffle-mobile.jpg'
+import mobilewaffle from '/image-waffle-desktop.jpg'
+import cartimage from '/icon-add-to-cart.svg'
+import baklava from '/image-baklava-mobile.jpg'
+import mobilebaklava from '/image-baklava-desktop.jpg'
+import meringue from '/image-meringue-mobile.jpg'
+import mobilemeringue from '/image-meringue-desktop.jpg'
+import panna from '/image-panna-cotta-mobile.jpg'
+import mobilepanna from '/image-panna-cotta-desktop.jpg'
+import cake from '/image-cake-mobile.jpg'
+import mobilecake from '/image-cake-desktop.jpg'
+import brulee from '/image-creme-brulee-mobile.jpg'
+import mobilebrulee from '/image-creme-brulee-desktop.jpg'
+import tiramisu from '/image-tiramisu-mobile.jpg'
+import mobiletiramisu from '/image-tiramisu-desktop.jpg'
+import macaron from '/image-macaron-mobile.jpg'
+import mobilemacaron from '/image-macaron-desktop.jpg'
+
 
 function App() {
+  console.log();
+  
   const product = [{
-    image:"/img/image-waffle-tablet.jpg",
+    desktopimg:mobilewaffle,
+    mobileimg: waffle,
      name:"waffle",
-     description:"affle with berries",
+     desc:"affle with berries",
      price:650
-  }]
+  },{
+    desktopimg:mobilebaklava,
+    mobileimg:baklava,
+     name:" Baklava",
+     desc:"Pistachio Baklava",
+     price:400
+     
+},{
+    desktopimg:mobilemeringue,
+    mobileimg:meringue,
+     name:"pie",
+     desc:"lemon Meringue pie",
+     price:500
+},{
+      desktopimg:mobilepanna,
+      mobileimg:panna,
+     name:"Panna Cotta",
+     desc:" Vanilla Panna Cotta",
+     price:650
+},
+{ 
+     desktopimg: brownie2,
+     mobileimg: brownie,
+     name:"Brownie",
+     desc:"Salted Caramel Brownie",
+     price:550  
+},{
+     desktopimg:mobilecake,
+     mobileimg:cake,
+     name:"Cake",
+     desc:"Red Velvet Cake",
+     price:450 
+},{
+    desktopimg:mobilebrulee,
+    mobileimg:brulee,
+     name:" Creme Brulee",
+     desc:"Vanilla Bean Creme Brulee",
+     price:700
+},{
+    desktopimg:mobiletiramisu,
+    mobileimg:tiramisu,
+     name:" Tiramisu",
+     desc:"Classic Tiramisu",
+     price:550
+},{
+    desktopimg:mobilemacaron,
+    mobileimg:macaron,
+     name:" Macaron",
+     desc:"Macaron Mix of Five",
+     price:800
+}]
 
   return (
     <>
-    <Card/>
+    <div className='flex justify-center'>
+    <Card product ={product}/>
+    </div>
     </>
   )
 }
--- product-cart/src/components/page.jsx
+++ product-cart/src/components/page.jsx
@@ -1,27 +1,77 @@
 import waffle from '/image-waffle-desktop.jpg'
 import mobilewaffle from '/image-waffle-mobile.jpg'
 import cartimage from '/icon-add-to-cart.svg'
-function Card() {
-    return(
-        <div className=' m-5'>
-            <h1>Dessert</h1>
-        <div className=' md:block'>
-            <div  className="relative md:w-[18rem] mt-5">
-                <img className='w-[14rem] hidden md:block' src={waffle} alt="" />
-                <img className='md:hidden' src={mobilewaffle} alt="" />
-                <div className='absolute -bottom-4 left-1/2  -translate-x-1/2 md:left-1/4 transform md:-translate-x-1/4'>
-                    <button   class="addBtn bg-[#fdfdfe] shadow-lg px-6 py-2 rounded-2xl flex items-center justify-center gap-2 whitespace-nowrap">
-                     <img  class="w-6 h-6" src={cartimage} alt="" />
-                   <span class="text-sm font-medium">Add to cart</span>
-                   </button>
+function Card({product}) {
+    console.log(product);
+    
+    
+    return (
+    <div className="m-5">
+      <h1 className="text-3xl font-bold mb-6">Dessert</h1>
+
+      <div className="flex flex-col  md:flex-row ">
+       
+        <div className="flex flex-col flex-1">
+          <div className="grid grid-cols-1 md:grid-cols-3 ">
+            {product.map((item, index) => (
+              <div key={index} className="md:block">
+                <div className="relative md:w-[18rem] mt-5">
+                  <img
+                    className="w-[14rem] hidden md:block"
+                    src={item.desktopimg}
+                    alt={item.name}
+                  />
+                  <img
+                    className="md:hidden"
+                    src={item.mobileimg}
+                    alt={item.name}
+                  />
+                  <div className="absolute -bottom-4 left-1/2 -translate-x-1/2 md:left-1/4 transform md:-translate-x-1/4">
+                    <button className="addBtn bg-[#fdfdfe] shadow-lg px-6 py-2 rounded-2xl flex items-center justify-center gap-2 whitespace-nowrap">
+                      <img className="w-6 h-6" src={cartimage} alt="cart" />
+                      <span className="text-sm font-medium">Add to cart</span>
+                    </button>
+                  </div>
                 </div>
-            </div>
-            <div class=" mt-7">
-          <p class="text-[#c9b6b4]">Waffle</p>
-          <p class="font-semibold">affle with berries</p>
-          <span class="text-orange-600">$6.50</span>
+                <div className="mt-7">
+                  <p className="text-[#c9b6b4]">{item.name}</p>
+                  <p className="font-semibold">{item.desc}</p>
+                  <span className="text-orange-600">
+                    ${(item.price / 100).toFixed(2)}
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
+
+        
+        <div className="hidden bg-white w-[20rem] rounded-lg shadow-xl md:block h-fit py-5 shrink-0">
+          <p className="text-orange-700 font-semibold mx-5 text-2xl mt-5">
+            Your Cart (<span className="addToCart">0</span>)
+          </p>
+          <div className="cart-display cart-items-js mx-5 mt-3" />
+          <div className="empty-cart-text text-center mt-40 px-4">
+            <p className="text-orange-700">Your added items will be here</p>
+          </div>
         </div>
+      </div>
+
+    
+      <div className="px-4 md:hidden mt-10">
+        <div
+          id="cart-mobile"
+          className="bg-white rounded-lg shadow-xl py-5 shrink-0 overflow-auto"
+        >
+          <p className="text-orange-700 font-semibold mx-5 text-2xl mt-5">
+            Your Cart (<span className="addToCart-mobile">0</span>)
+          </p>
+          <div className="cart-display mx-5 mt-3" />
+          <div className="empty-cart-text text-center mt-40 px-4">
+            <p className="text-orange-700">Your added items will be here</p>
+          </div>
+        </div>
+      </div>
     </div>
     )
 }
